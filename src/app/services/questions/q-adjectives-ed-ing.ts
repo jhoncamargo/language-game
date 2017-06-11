1b13fccf928cdf6f711ce3b1462017a2da58bd30
Jhon Camargo <jhoncamargo@seven4n.com>
2017-06-11 17:59:49 -0500

    add questions ending with ing or ed

---
+++ src/app/services/questions/q-adjectives-ed-ing.ts
@@ -0,0 +1,14 @@
+import { Question } from '../../model/question';
+
+export const ADJECTIVES_ED_ING: Question[] = [
+  {question: 'Sit down - I\'ve got some very (excited/exciting) news for you.' , response: 'exciting'},
+  {question: 'The findings of this report are (confused/confusing).', response: 'confusing'},
+  {question: 'It was the most (excited/exciting) I have been watching a film', response: 'excited'},
+  {question: 'He\'s got a very (annoyed/annoying) habit of always interrupting people.', response: 'annoying'},
+  {question: 'I\'m very (disappointed/disappointing) by your behavior.', response: 'disappointed'},
+  {question: 'Kids! You\'re (disgusted/disgusting)! Don\'t talk with your mouths full!.', response: 'disgusting'},
+  {question: 'Amanda was (shocked/shocking) to hear about the earthquake.', response: 'shocked'},
+  {question: 'The meals at Immigrant\'s Cafe are (satisfied/satisfying)', response: 'satisfying'},
+  {question: 'Your speech was very (inspired/inspiring).', response: 'inspiring'},
+  {question: 'You look really (tired/tiring). Why don\'t you go to bed.', response: 'tired'},
+];
